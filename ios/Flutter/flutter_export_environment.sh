ffd7d15fb2754a6a1146f4880dd323504db0df7e
Emanuel Vilela <usermane13@gmail.com>
2022-08-26 15:51:48 -0300

    transferindo repositório e salvando usuário no lab do dit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\asus\AppData\Local\Android\FlutterSdk"
-export "FLUTTER_APPLICATION_PATH=C:\Users\asus\Desktop\new_codebases\navigation_over_screens"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\userm\StudioProjects\whatinif"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:whatinif/src/pages/login_page.dart';
 import 'package:whatinif/src/pages/register_page.dart';
 import 'package:whatinif/src/pages/user/edit_user_page.dart';
-<<<<<<< HEAD
-import 'package:whatinif/src/pages/vaga/info_vaga.dart';
 import 'package:whatinif/src/pages/vaga/register_vaga_page.dart';
-<<<<<<< HEAD
-=======
-=======
 import 'package:whatinif/src/pages/vaga/info_vaga_page.dart';
->>>>>>> 76223a7f5129e4579a4b3ffcd2c323c46fe4a3bd
->>>>>>> 8b7423dcb4051855237fb3b6d7622e916b6f11e0
 import 'src/pages/user/user_page.dart';
 import 'src/pages/home_page.dart';
 import 'src/widget/bottom_bar_navigator.dart';
