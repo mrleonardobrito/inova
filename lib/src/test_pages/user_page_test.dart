45decd97ea5be683bfae2815350d2f54a388aee4
Emanuel Vilela <84058517+emanuelvsz@users.noreply.github.com>
2022-10-11 11:29:28 -0300

    Add files via upload

---
+++ lib/src/test_pages/user_page_test.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class UserPageTest extends StatefulWidget {
+  const UserPageTest({Key? key}) : super(key: key);
+
+  @override
+  _UserPageTestState createState() => _UserPageTestState();
+}
+
+
+class _UserPageTestState extends State<UserPageTest> {
+  @override
+  Widget build(BuildContext context) {
+
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Container(
+          height: height,
+          width: width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: height * 0.07,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFFFFFF)
+                    ),
+                  ),
+                  Container(
+                    height: height * 0.13,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF4065FC)
+                    ),
+                  ),
+                  Container(
+                    height: height * 0.21,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFFFFFF)
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    height: height * 0.07,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF4065FC)
+                    ),
+                  ),
+                  Container(
+                    height: height * 0.42,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFE8ECFF)
+                    ),
+                  ),
+                  Container(
+                    height: height * 0.10,
+                    width: width,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFFFFFF)
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: (height * 0.11) * 1.35,
+                left: width * 0.05,
+                child: ClipRRect(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.black),
+                    child: Image.asset(
+                      "images/xereque.png",
+                      height: height * 0.11,
+                      width: height * 0.11,
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(150),
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
