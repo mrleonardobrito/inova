fc1af60c9e1b48ed8ba2f7cdbceb735abeb24ecd
aluno <usermane13@gmail.com>
2022-10-31 16:14:13 -0300

    INOVA-4-...

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:inova/src/pages/home/home_screen.dart';
+import 'package:inova/src/utils/routes.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomeScreen(),
+      home: Routes(),
     ),
   );
 }
---
+++ lib/src/utils/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Routes extends StatefulWidget {
+  const Routes({Key? key}) : super(key: key);
+
+  @override
+  _RoutesState createState() => _RoutesState();
+}
+
+class _RoutesState extends State<Routes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('não'));
+  }
+}
