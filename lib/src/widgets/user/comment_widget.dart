04960f0b70b9c91c69b154a42108d7a8b7800b3c
emanuelvsz <usermane13@gmail.com>
2022-11-27 15:38:39 -0300

    INOVA-5: removendo problemas do widget comments

--- lib/src/widgets/user/comment_widget.dart
+++ lib/src/widgets/user/comment_widget.dart
@@ -5,18 +5,15 @@ class CommentsWidget extends StatefulWidget {
   const CommentsWidget({Key? key}) : super(key: key);
 
   @override
-  _CommentsWidgetState createState() => _CommentsWidgetState();
+  CommentsWidgetState createState() => CommentsWidgetState();
 }
 
-class _CommentsWidgetState extends State<CommentsWidget> {
+class CommentsWidgetState extends State<CommentsWidget> {
   int showAbas = 1;
-  bool _isInitialValue = true;
 
-  int _myDuration = 2;
-
-  Widget star = Icon(Icons.star, size: 12, color: Color(0xFF4065FC));
-  double _commentOpacity = .5;
-  bool _showGhostCard = false;
+  Widget star = const Icon(Icons.star, size: 12, color: Color(0xFF4065FC));
+  double commentOpacity = .5;
+  bool showGhostCard = false;
 
   String commentText =
       'Trabalho sensacional feito pelo aluno. Esse mlk merece 5k de salário pq ele é foda. O melhor aluno da minha vida. Foram 5 meses desenvolvendo o aplicativo e pipipipopopo e tals tals tals ';
@@ -26,151 +23,166 @@ class _CommentsWidgetState extends State<CommentsWidget> {
     return Stack(
       children: [
         Opacity(
-            opacity: _showGhostCard ? _commentOpacity : 1,
+          opacity: showGhostCard ? commentOpacity : 1,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                showGhostCard = !showGhostCard;
+              });
+            },
+            onDoubleTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const CommentsPage()),
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width - 20,
+              decoration: const BoxDecoration(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF4065FC),
+                                border: Border.all(
+                                  width: 1,
+                                  color: const Color(0xFF4065FC),
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(60),
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(150),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      color: Colors.black),
+                                  child: Image.asset(
+                                    "images/xereque.png",
+                                    height: 40,
+                                    width: 40,
+                                    fit: BoxFit.fitHeight,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(left: 5),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Tarsis Marinho',
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF4065FC)),
+                                  ),
+                                  const Text(
+                                    'Professor',
+                                    style: TextStyle(
+                                        color: Color(0xFF8599EA),
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Positioned(
+                                    left: 100,
+                                    child: Row(
+                                      children: [
+                                        Container(child: star),
+                                        Container(child: star),
+                                        Container(child: star),
+                                        Container(child: star),
+                                        Container(child: star),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          width: MediaQuery.of(context).size.width - 20,
+                          color: Colors.transparent,
+                          child: Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: Text(
+                              commentText,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Opacity(
+          opacity: !showGhostCard ? 0 : 1,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width / 2 - 80,
+              top: 67,
+            ),
             child: GestureDetector(
               onTap: () {
                 setState(() {
-                  _showGhostCard = !_showGhostCard;
-                  print(_showGhostCard);
+                  showGhostCard = !showGhostCard;
                 });
               },
               onDoubleTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => CommentsPage()),
+                  MaterialPageRoute(builder: (context) => const CommentsPage()),
                 );
               },
               child: Container(
-                  margin: EdgeInsets.only(top: 20),
-                  width: MediaQuery.of(context).size.width - 20,
-                  decoration: BoxDecoration(),
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Color(0xFFF5F5F5),
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 40,
-                                    width: 40,
-                                    decoration: BoxDecoration(
-                                        color: Color(0xFF4065FC),
-                                        border: Border.all(
-                                            width: 1,
-                                            color: const Color(0xFF4065FC)),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(60))),
-                                    child: ClipRRect(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(100),
-                                            color: Colors.black),
-                                        child: Image.asset(
-                                          "images/xereque.png",
-                                          height: 40,
-                                          width: 40,
-                                          fit: BoxFit.fitHeight,
-                                        ),
-                                      ),
-                                      borderRadius: BorderRadius.circular(150),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(left: 5),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'Tarsis Marinho',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              fontWeight: FontWeight.bold,
-                                              color: Color(0xFF4065FC)),
-                                        ),
-                                        Text(
-                                          'Professor',
-                                          style: TextStyle(
-                                              color: Color(0xFF8599EA),
-                                              fontSize: 11,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Positioned(
-                                          left: 100,
-                                          child: Row(
-                                            children: [
-                                              Container(child: star),
-                                              Container(child: star),
-                                              Container(child: star),
-                                              Container(child: star),
-                                              Container(child: star),
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(top: 10),
-                                width: MediaQuery.of(context).size.width - 20,
-                                color: Colors.transparent,
-                                child: Padding(
-                                  padding: EdgeInsets.all(5),
-                                  child: Text(
-                                    commentText,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ))),
-            )),
-        Opacity(
-            opacity: !_showGhostCard ? 0 : 1,
-            child: Padding(
-                padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width / 2 - 80, top: 67),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _showGhostCard = !_showGhostCard;
-                      print(_showGhostCard);
-                    });
-                  },
-                  onDoubleTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => CommentsPage()),
-                    );
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    decoration: BoxDecoration(
-                        color: Color(0xFF4065FC),
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
-                    child: Center(
-                      child: Text(
-                        'Clique duas vezes',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
+                height: 50,
+                width: 150,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF4065FC),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Clique duas vezes',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                )))
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
