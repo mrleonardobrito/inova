bdeb9ea2f72150419cc4ef3745bf54105660d3df
emanuelvsz <usermane13@gmail.com>
2022-11-27 15:45:49 -0300

    INOVA-5: removendo problemas da tela main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:inova/src/pages/home/home_screen.dart';
 import 'package:inova/src/pages/login/login_page.dart';
-import 'package:inova/src/pages/register/register_page.dart';
 
 void main() {
   runApp(
