1a595332ba2945d5e0bdf1c6ca1c4dcaa31bf4b8
emanuelvsz <usermane13@gmail.com>
2022-12-19 15:46:58 -0300

    INOVA-5: solving db_helper problem

--- lib/src/data/db_helpers/db_helper.dart
+++ lib/src/data/db_helpers/db_helper.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:path/path.dart';
 import 'dart:io' show Directory, File;
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
